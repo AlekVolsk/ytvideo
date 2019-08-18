cfd778ccf88204a9a83e21cdc86bca45e07b6b1e
AlekVolsk <alekvolsk@yandex.ru>
2019-08-19 03:21:22 +0400

    v1.5.3

--- plg_content_ytvideo/assets/ytvideo.js
+++ plg_content_ytvideo/assets/ytvideo.js
@@ -4,11 +4,26 @@
  * @copyright   Copyright (C) 2019 Aleksey A. Morozov. All rights reserved.
  * @license     GNU General Public License version 3 or later; see http://www.gnu.org/licenses/gpl-3.0.txt
  */
+
+(function () {
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (callback, thisArg) {
+            thisArg = thisArg || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(thisArg, this[i], i, this);
+            }
+        };
+    }
+    if (typeof NodeList.prototype.forEach !== "function") {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+})();
+
 document.addEventListener('DOMContentLoaded', function () {
-    if (document.querySelector('.ytvideo > .ytvideo-cover')) {
-        document.querySelector('.ytvideo > .ytvideo-cover').addEventListener('click', function (e) {
+    document.querySelectorAll('.ytvideo > .ytvideo-cover').forEach(function (el) {
+        el.addEventListener('click', function (e) {
             var target = e.target || e.srcElement;
             target.innerHTML = "<iframe src='//youtube.com/embed/" + target.dataset.videosrc + "?autoplay=1&rel=0&iv_load_policy=3' allow='accelerometer;autoplay;encrypted-media;gyroscope;picture-in-picture' allowfullscreen style='border:0;'></iframe>";
         });
-    }
+    });
 });
